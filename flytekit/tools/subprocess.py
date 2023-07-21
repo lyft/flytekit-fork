cd8da0046a3e03c57e9958c05c8ff95cd1d190ba
Catalin Toda <catalinii@gmail.com>
2023-07-21 11:26:10 -0700

    Dump output from subprocess to stdout/stderr

--- flytekit/tools/subprocess.py
+++ flytekit/tools/subprocess.py
@@ -7,23 +7,18 @@ import tempfile as _tempfile
 def check_call(cmd_args, **kwargs):
     if not isinstance(cmd_args, list):
         cmd_args = _schlex.split(cmd_args)
+    print("Running {cmd_args}")
+    
+    p = _subprocess.Popen(cmd_args, **kwargs)
+    _, _ = p.communicate()
 
-    # Jupyter notebooks hijack I/O and thus we cannot dump directly to stdout.
-    with _tempfile.TemporaryFile() as std_out:
-        with _tempfile.TemporaryFile() as std_err:
-            ret_code = _subprocess.Popen(cmd_args, stdout=std_out, stderr=std_err, **kwargs).wait()
+    # Dump sub-process' std out into current std out
 
-            # Dump sub-process' std out into current std out
-            std_out.seek(0)
-            logging.info("Output of command '{}':\n{}\n".format(cmd_args, std_out.read()))
+    if p.returncode != 0:
+        logging.error("Error from command '{}':\n{}\n".format(cmd_args, ret_code))
 
-            if ret_code != 0:
-                std_err.seek(0)
-                err_str = std_err.read()
-                logging.error("Error from command '{}':\n{}\n".format(cmd_args, err_str))
-
-                raise Exception(
-                    "Called process exited with error code: {}.  Stderr dump:\n\n{}".format(ret_code, err_str)
-                )
+        raise Exception(
+            "Called process exited with error code: {}.  Stderr dump:\n\n{}".format(ret_code, err_str)
+        )
 
     return 0
